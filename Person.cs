4f99bee7d6faec196a9ff2022b433ca06544173b
Pavel Sadykov <2439747@gmail.com>
2023-04-09 11:59:33 +0500

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace test2
+{
+	public class Person
+	{
+		private string name;
+		private int age;
+		public int Age { get; set; }
+		public string Name { /*set;*/ get { return name; }}
+		
+		public void Print()
+		{
+			Console.WriteLine($"Имя {Name} возраст {Age}");
+           // Console.WriteLine("Имя {0} возраст {1}",Name, Age);// то же самое 
+        }
+		public Person(string _name)
+		{
+			name = _name;
+		}	
+		
+	}
+	}
+
+
